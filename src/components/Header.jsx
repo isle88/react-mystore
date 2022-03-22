ab82499462b0d91934e77b7af62f5e8723a5916b
isle88 <hyunjungkim2014@gmail.com>
2022-03-22 11:35:34 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 >MY STORE</h1>
+        </div>
+    );
+};
+
+export default Header;
