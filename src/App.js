0677ef2d682b62bf2533d94a27707d8dc0b40417
isle88 <hyunjungkim2014@gmail.com>
2022-03-24 17:02:14 +0000

    misc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Product from './components/Product';
-import Products from './components/Products';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-     <Header/>
-     <Routes>
-      <Route path='/' element={<Products />} />
-      <Route path='/products/:id' element={<Product />} />
-     </Routes>
-     <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
