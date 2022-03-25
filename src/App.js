bcf8dc7de19f949ebab0ecd8ceb9d0f2c9c8803f
isle88 <hyunjungkim2014@gmail.com>
2022-03-25 19:00:17 +0000

    add context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Product from "./components/Product";
+import Like from "./components/Like";
+import Product from "./components/ProductCard";
 import Products from "./components/Products";
+import { CartContext } from "./contexts/CartContext";
+import { LikeContext } from "./contexts/LikeContext";
 
 function App() {
+  // const storedLike = JSON.parse(sessionStorage.getItem("liked"));
+  // const storedCart = JSON.parse(sessionStorage.getItem("cart"));
+
+  const [liked, setLiked] = useState([]);
+  const [cart, setCart] = useState([]);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/products/:id" element={<Product />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <LikeContext.Provider value={{ liked, setLiked }}>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <div className="App">
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Products />} />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/like" element={<Like />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </div>
+      </CartContext.Provider>
+    </LikeContext.Provider>
   );
 }
 
