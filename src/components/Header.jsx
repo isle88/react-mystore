e809829e72c183218a36902fcb71dd4389d42b66
isle88 <hyunjungkim2014@gmail.com>
2022-03-25 19:00:46 +0000

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,67 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import { CartContext } from "../contexts/CartContext";
+import { LikeContext } from "../contexts/LikeContext";
 
 const Header = () => {
+  const { liked, setLiked } = useContext(LikeContext);
+  const { cart, setCart } = useContext(CartContext);
+
+  const storedLike = JSON.parse(sessionStorage.getItem("liked"));
+  const storedCart = JSON.parse(sessionStorage.getItem("cart"));
+  
+  // console.log(storedLike, 'storedCart')
+  // console.log(liked, 'liked')
+ 
+  // const productId = product.id.toString();
+
+  // console.log(storedCart, 'storecart')
+  // console.log(liked)
+
+  useEffect(() => {
+
+    if(storedLike !== null) {
+      const likeId = liked.map((item) => item.id).toString();
+      console.log(likeId, 'likeId')
+      
+      setLiked(storedLike)
+    }
+
+    if(storedCart !== null) {
+      setCart(storedCart)
+    }
+  }, [setLiked, setCart])
+
   return (
     <div>
       {/* <Navbar fixed="top" /> */}
-      <Navbar className='fixed-top' bg="dark" variant="dark" style={{ maxHeight: '45px' }}>
+      <Navbar
+        className="fixed-top"
+        bg="dark"
+        variant="dark"
+        style={{ maxHeight: "45px" }}
+      >
         <Container>
           <Navbar.Brand href="/">MY STORE</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#products">Product</Nav.Link>
-            <Nav.Link href="#like">Like</Nav.Link>
+            <Nav.Link href="/like">
+              Like{" "}
+              {liked.length > 0 && (
+                <Badge pill bg="light" text="dark">
+                  {" "}
+                  {storedLike.length}
+                </Badge>
+              )}{" "}
+            </Nav.Link>
+            <Nav.Link href="#cart">
+              Cart{" "}
+              {cart.length > 0  && (
+                <Badge pill bg="light" text="dark">
+                  {" "}
+                  {storedCart.length}
+                </Badge>
+              )}
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
