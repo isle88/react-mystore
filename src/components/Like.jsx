7b802b8535f3aa6174b702fc3c84b2277ce56f7f
isle88 <hyunjungkim2014@gmail.com>
2022-03-31 12:58:03 +0100

    update rendering

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,14 +1,47 @@
-import React, { useContext, useState } from "react";
-import { CartContext } from '../contexts/CartContext';
-import { LikeContext } from '../contexts/LikeContext';
+import React, { useContext, useEffect, useState } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import { LikeContext } from "../contexts/LikeContext";
 
 const Like = () => {
   const { liked, setLiked } = useContext(LikeContext);
-  const { cart, setCart } = useContext(CartContext);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (liked.length) {
+      setIsLoaded(true);
+    }
+  },[liked.length]);
 
   return (
     <div className="div">
-      <p>Like page</p>
+      <>
+        {isLoaded ? (
+          <>
+            <Card>
+              <Card.Header>Items you liked</Card.Header>
+              <ListGroup variant="flush">
+                {[...liked].map((product) => {
+                  return (
+                    <ListGroup.Item>
+                      {product.title}
+                      <br />£ {product.price}
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Card>
+          </>
+        ) : (
+          <>
+            <Card>
+              <Card.Header>Items you liked</Card.Header>
+              <ListGroup variant="flush">
+                <ListGroup.Item>please add item you liked first</ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </>
+        )}
+      </>
     </div>
   );
 };
